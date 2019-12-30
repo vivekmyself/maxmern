8d6e1a371619ae47c0bd88e14b3f22fcef79f1f6
Vivek Arockiaraj <vivek.arockiaraj@genomels.com>
2019-12-30 16:40:18 +0530

    upto input validation 58

--- fe/src/places/pages/NewPlace.js
+++ fe/src/places/pages/NewPlace.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React from "react";
+
+import Input from "../../shared/components/FormElements/Input";
+import { VALIDATOR_REQUIRE } from "../../shared/util/validators";
+import "./NewPlace.css";
 
 const NewPlace = () => {
-  return <h2>NewPlace Works!</h2>
+  return (
+    <form className="place-form">
+      <Input
+        type="text"
+        element="input"
+        label="myInput"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid title"
+      />
+    </form>
+  );
 };
 
 export default NewPlace;
--- fe/src/shared/components/Navigation/SideDrawer.js
+++ fe/src/shared/components/Navigation/SideDrawer.js
@@ -1,9 +1,24 @@
 import React from "react";
+import ReactDOM from "react-dom";
+import { CSSTransition } from "react-transition-group";
 
 import "./SideDrawer.css";
 
 const SideDrawer = props => {
-  return <aside>{props.children}</aside>;
+  const content = (
+    <CSSTransition
+      in={props.show}
+      timeout={200}
+      classNames="slide-in-left"
+      mountOnEnter
+      unmountOnExit
+    >
+      <aside className="side-drawer" onClick={props.onClick}>
+        {props.children}
+      </aside>
+    </CSSTransition>
+  );
+  return ReactDOM.createPortal(content, document.getElementById("drawerHook"));
 };
 
 export default SideDrawer;
