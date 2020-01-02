3ca1c458a55bc1854e6c5306a4f7f742938a782f
Vivek Arockiaraj <vivek.arockiaraj@genomels.com>
2020-01-02 15:52:13 +0530

    express server initialiazion 90

---
+++ routes/places-routes.js
@@ -0,0 +1,49 @@
+const express = require("express");
+
+const router = express.Router();
+
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empire State",
+    description: "Empire State Building world famous",
+    imageURL:
+      "https://untappedcities.com/wp-content/uploads/2015/07/Flatiron-Building-Secrets-Roof-Basement-Elevator-Sonny-Atis-GFP-NYC_5.jpg",
+    address: "2 Mannar St, NY",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878531
+    },
+    creator: "u1"
+  },
+  {
+    id: "p2",
+    title: "Empire State OK",
+    description: "Empire State Building world famous",
+    imageURL: "https://cdn.getyourguide.com/img/tour_img-1739965-148.jpg",
+    address: "5 Mannar St, NY",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878531
+    },
+    creator: "u2"
+  }
+];
+
+router.get("/:pid", (req, res, next) => {
+  const placeId = req.params.pid;
+  const place = DUMMY_PLACES.find(p => {
+    return p.id === placeId;
+  });
+  res.json({ place });
+});
+
+router.get("/user/:uid", (req, res, next) => {
+  const userId = req.params.uid;
+  const place = DUMMY_PLACES.find(p => {
+    return p.creator === userId;
+  });
+  res.json({ place });
+});
+
+module.exports = router;
